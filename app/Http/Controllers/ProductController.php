1f67b59af08a47a20cb0e27c21c87558d696e6a0
Aditya Gupta <mailadityaguptaa@gmail.com>
2024-10-12 12:32:37 +0530

    feat(product): add pagination, ProductFactory, and update tests
    - Implement pagination in ProductController
    - Create ProductFactory for generating fake product data
    - Update tests to use ProductFactory for creating fake products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,7 +8,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::all();
+        $products = Product::paginate(10);
         return view('products.index', compact('products'));
     }
 }
--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -3,6 +3,7 @@
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 use function Pest\Laravel\actingAs;
 use function Pest\Laravel\get;
 
@@ -25,7 +26,20 @@ test('homepage contains non empty table', function () {
     $user = User::factory()->create();
     actingAs($user)->get('/products')
         ->assertStatus(200)
-        ->assertDontSee(__('No products found'))->assertViewHas('products',function (Collection $collection) use ($product){
+        ->assertDontSee(__('No products found'))->assertViewHas('products',function (LengthAwarePaginator  $collection) use ($product){
             return $collection->contains($product);
+        });
+});
+
+test('paginated products table doesnt contain 11th record', function () {
+    $user = User::factory()->create();
+    $products  = Product::factory(11)->create();
+
+    $lastProduct = $products->last();
+
+    actingAs($user)->get('/products')
+        ->assertStatus(200)
+        ->assertViewHas('products', function (LengthAwarePaginator $collection) use ($lastProduct) {
+            return $collection->doesntContain($lastProduct);
         });
 });
